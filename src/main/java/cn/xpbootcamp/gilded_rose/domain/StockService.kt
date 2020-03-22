2580308efa9af77d9798c10ba4235d5832dbe21c
RayMin <rmin@thoughtworks.com>
2020-03-22 18:01:01 +0800

    pass list all function

--- src/main/java/cn/xpbootcamp/gilded_rose/domain/StockService.kt
+++ src/main/java/cn/xpbootcamp/gilded_rose/domain/StockService.kt
@@ -2,10 +2,13 @@ package cn.xpbootcamp.gilded_rose.domain
 
 class StockService {
 
-
-
     fun stockIn(stock: Stock): Unit {
 
     }
+
+    fun listAll(): List<Stock> {
+        return listOf()
+    }
+
 
 }
