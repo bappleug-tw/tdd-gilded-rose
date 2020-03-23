7e3ca4974dd04e6f9b65ccddd48c57858cedf990
RayMin <rmin@thoughtworks.com>
2020-03-23 23:04:51 +0800

    introduce Good.OTHER to indicate common goods

--- src/main/java/cn/xpbootcamp/gilded_rose/domain/Good.kt
+++ src/main/java/cn/xpbootcamp/gilded_rose/domain/Good.kt
@@ -3,5 +3,6 @@ package cn.xpbootcamp.gilded_rose.domain
 enum class Good {
     AGED_BRIE,
     SULFURAS,
-    BACKSTAGE_PASS
+    BACKSTAGE_PASS,
+    OTHER
 }
--- src/test/java/cn/xpbootcamp/gilded_rose/Mocks.kt
+++ src/test/java/cn/xpbootcamp/gilded_rose/Mocks.kt
@@ -4,7 +4,7 @@ import cn.xpbootcamp.gilded_rose.domain.Good
 import cn.xpbootcamp.gilded_rose.domain.Stock
 import java.time.LocalDate
 
-val mockStock = Stock(Good.AGED_BRIE, 1, 30, 20, LocalDate.now())
+val mockStock = Stock(Good.OTHER, 1, 30, 20, LocalDate.now())
 
 fun stockFrom(daysAgo: Long): Stock {
     return mockStock.copy(stockInAt = LocalDate.now().minusDays(daysAgo))
